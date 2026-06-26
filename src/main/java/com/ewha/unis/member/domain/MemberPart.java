914f8449a109f61d73656c6b7f7cd9502e679b4f
nomellc <jyj100743@ewha.ac.kr>
2026-06-26 19:45:37 +0900

    feat: Member 관련 Enum 생성

---
+++ src/main/java/com/ewha/unis/member/domain/MemberPart.java
@@ -0,0 +1,5 @@
+package com.ewha.unis.member.domain;
+
+public enum MemberPart {
+    PLANNING, DESIGN, FRONTEND, BACKEND
+}
---
+++ src/main/java/com/ewha/unis/member/domain/Role.java
@@ -0,0 +1,5 @@
+package com.ewha.unis.member.domain;
+
+public enum Role {
+    USER, ADMIN
+}
