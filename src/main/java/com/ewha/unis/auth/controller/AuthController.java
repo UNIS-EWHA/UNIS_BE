9956050f45faf20c580bf53a402d16346f5dfb93
nomellc <jyj100743@ewha.ac.kr>
2026-06-27 22:33:44 +0900

    feat: 회원가입 서비스 및 컨트롤러 작성

--- src/main/java/com/ewha/unis/auth/controller/AuthController.java
+++ src/main/java/com/ewha/unis/auth/controller/AuthController.java
@@ -1,16 +1,15 @@
 package com.ewha.unis.auth.controller;
 
 import com.ewha.unis.auth.dto.LoginIdCheckResponse;
+import com.ewha.unis.auth.dto.SignUpRequest;
 import com.ewha.unis.auth.service.AuthService;
 import com.ewha.unis.global.response.code.SuccessCode;
 import com.ewha.unis.global.response.dto.BaseResponse;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -23,5 +22,12 @@ public class AuthController {
     public BaseResponse<LoginIdCheckResponse> checkAvailableLoginId(
             @RequestParam @NotBlank String loginId) {
         return BaseResponse.ok(authService.checkAvailableLoginId(loginId));
+    }
+
+    @PostMapping("/signup")
+    public BaseResponse<Void> signUp(
+            @Valid @RequestBody SignUpRequest request) {
+        authService.signUp(request);
+        return BaseResponse.created();
     }
 }
