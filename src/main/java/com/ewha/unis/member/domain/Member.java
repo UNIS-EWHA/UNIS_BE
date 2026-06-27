69d3988721683cdd17582c32295c0761b290c694
nomellc <jyj100743@ewha.ac.kr>
2026-06-27 21:51:19 +0900

    feat: Member 엔티티 빌더 추가

--- src/main/java/com/ewha/unis/member/domain/Member.java
+++ src/main/java/com/ewha/unis/member/domain/Member.java
@@ -3,8 +3,11 @@ package com.ewha.unis.member.domain;
 import com.ewha.unis.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -37,4 +40,24 @@ public class Member extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    @Column(nullable = false)
+    private LocalDateTime termsAgreedAt;
+
+    @Column(nullable = false)
+    private LocalDateTime privacyAgreedAt;
+
+    @Builder
+    private Member(String name, String email, String loginId, String encodedPassword,
+                   Integer generation, MemberPart part) {
+        this.name = name;
+        this.email = email;
+        this.loginId = loginId;
+        this.password = encodedPassword;
+        this.generation = generation;
+        this.part = part;
+        this.role = Role.USER;
+        this.termsAgreedAt = LocalDateTime.now();
+        this.privacyAgreedAt = LocalDateTime.now();
+    }
 }
