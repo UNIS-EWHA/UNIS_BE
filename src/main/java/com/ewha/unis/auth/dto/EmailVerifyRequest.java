889a87cc42a3c3fbc5451346b530b49693f52c74
nomellc <jyj100743@ewha.ac.kr>
2026-06-27 16:45:11 +0900

    feat: 이메일 인증 dto 생성

---
+++ src/main/java/com/ewha/unis/auth/dto/EmailVerifyRequest.java
@@ -0,0 +1,14 @@
+package com.ewha.unis.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record EmailVerifyRequest(
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String code
+) { }
