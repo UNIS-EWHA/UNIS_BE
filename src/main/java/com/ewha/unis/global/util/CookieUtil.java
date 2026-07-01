c2e7ad087dab687245f361669d845ed688ef8f0c
nomellc <jyj100743@ewha.ac.kr>
2026-07-02 00:42:49 +0900

    feat: HttpOnly 쿠키 유틸 구현

---
+++ src/main/java/com/ewha/unis/global/util/CookieUtil.java
@@ -0,0 +1,21 @@
+package com.ewha.unis.global.util;
+
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class CookieUtil {
+    public static final String REFRESH_TOKEN = "refreshToken";
+
+    public ResponseCookie create(String value, long validityMs) {
+        return ResponseCookie.from(REFRESH_TOKEN, value)
+                .httpOnly(true)
+                .secure(false) // TODO: HTTPS 세팅 후 변경
+                .sameSite("Lax")
+                .path("/api/v1/auth")
+                .maxAge(Duration.ofMillis(validityMs))
+                .build();
+    }
+}
