d2f023d2169a440054958e2fba2c7c7c6d1f5517
구아연 <kooayeon@ewha.ac.kr>
2026-07-30 02:10:38 +0900

    fix: application-prod.yml 최초 커밋 누락분 추가

--- src/main/java/com/ewha/unis/global/config/jwt/JwtProperties.java
+++ src/main/java/com/ewha/unis/global/config/jwt/JwtProperties.java
@@ -1,7 +1,10 @@
 package com.ewha.unis.global.config.jwt;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Slf4j
 @ConfigurationProperties(prefix = "jwt")
 public record JwtProperties(
         String secret,
@@ -9,4 +12,9 @@ public record JwtProperties(
         long refreshTokenValidity,
         long refreshTokenValidityShort
 ) {
+    @PostConstruct
+    public void logProperties() {
+        log.info("JwtProperties bound: accessTokenValidity={}, refreshTokenValidity={}, refreshTokenValidityShort={}",
+                accessTokenValidity, refreshTokenValidity, refreshTokenValidityShort);
+    }
 }
